a54ca986aade4d61d25227015b44cf7330d5a19e
GChicarelli <giulianochicarelli@gmai.com>
2026-03-26 10:45:56 -0300

    Atualização de dados

---
+++ GestaoPatrimonios/Applications/Services/LocalizacaoSevice.cs
@@ -0,0 +1,73 @@
+﻿using GestaoPatrimonios.Applications.Regras;
+using GestaoPatrimonios.Domains;
+using GestaoPatrimonios.DTOs.LocalizacaoDto;
+using GestaoPatrimonios.Exceptions;
+using GestaoPatrimonios.Interfaces;
+
+namespace GestaoPatrimonios.Applications.Services
+{
+    public class LocalizacaoService
+    {
+        private readonly ILocalizacaoRepository _repository;
+        private object localizacaoDto;
+
+        public LocalizacaoService(ILocalizacaoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public List<ListarLocalizacaoDto> Listar()
+        {
+            List<Localizacao> localizacoes = _repository.Listar();
+
+            List<ListarLocalizacaoDto> localizacoesDto = localizacoes.Select(local => new ListarLocalizacaoDto
+            {
+                LocalizacaoID = local.LocalizacaoID,
+                NomeLocal = local.NomeLocal,
+                LocalSAP = local.LocalSAP,
+                DescricaoSAP = local.DescricaoSAP,
+                AreaID = local.AreaID
+            }).ToList();
+
+            return localizacoesDto;
+        }
+
+        public ListarLocalizacaoDto BuscarPorId(Guid localizacaoId)
+        {
+            Localizacao localizacao = _repository.BuscarPorId(localizacaoId);
+
+            if (localizacao == null)
+            {
+                throw new DomainException("Localização não encontrada");
+            }
+
+            return new ListarLocalizacaoDto
+            {
+                LocalizacaoID = localizacao.LocalizacaoID,
+                NomeLocal = localizacao.NomeLocal,
+                LocalSAP = localizacao.LocalSAP,
+                DescricaoSAP = localizacao.DescricaoSAP,
+                AreaID = localizacao.AreaID
+            };
+        }
+
+        public object Adicionar (CriarLocalizacaoDto dto)
+        {
+            Validar.ValidarNome(dto.NomeLocal);
+
+            Localizacao localizacao = new Localizacao
+            {
+                NomeLocal = dto.NomeLocal,
+                LocalSAP = dto.LocalSAP,
+                DescricaoSAP = dto.DescricaoSAP,
+                AreaID = localizacao.AreaID,
+            };
+            return localizacaoDto;
+        }
+
+        internal void Atualizar(Guid id, CriarLocalizacaoDto dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GestaoPatrimonios/Repositories/LocalizaçaoRepository.cs
@@ -0,0 +1,38 @@
+﻿using GestaoPatrimonios.Contexts;
+using GestaoPatrimonios.Domains;
+using GestaoPatrimonios.DTOs.LocalizacaoDto;
+using GestaoPatrimonios.Interfaces;
+
+namespace GestaoPatrimonios.Repositories
+{
+    public class LocalizacaoRepository : ILocalizacaoRepository
+    {
+        private readonly GestaoPatrimoniosContext _context;
+
+        public LocalizacaoRepository(GestaoPatrimoniosContext context)
+        {
+            _context = context;
+        }
+
+        public List<Localizacao> Listar()
+        {
+            return _context.Localizacao.OrderBy(local => local.NomeLocal).ToList();
+        }
+
+        public Localizacao BuscarPorId(Guid localizacaoId)
+        {
+            return _context.Localizacao.Find(localizacaoId);
+        }
+
+        public void Adicionar(Localizacao localizacao)
+        {
+            _context.Localizacao.Add(localizacao);
+            _context.SaveChanges();
+        }
+
+        public bool AreaExiste(Guid areaId)
+        {
+            return _context.Area.Any(area => area.AreaID == areaId);
+        }
+    }
+}
